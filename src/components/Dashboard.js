9cf51bc920da6f390da6c8ee6d1f99cdf1551baa
Regina Mc Guire <rmcguire@antelecom.net>
2019-05-21 09:13:24 -0700

    Add Search Panel

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import Header from './Header'
-
+import SearchPanel from './SearchPanel'
 
 const Dashboard =() => (
     <div>
-        <Header />     
+        <Header />
+        <SearchPanel />
     </div>
 )
 export default Dashboard;
---
+++ src/components/SearchPanel.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+const SearchPanel =() => (
+    <div className="search-panel">
+        <form className="search-bar">
+        <input />
+        <button className="button">Search</button>
+        </form> 
+    </div>
+)
+export default SearchPanel;
