1dbcc1a8246e3bb07b88efe847a43d492a224f24
Regina McGuire <rmcguire@as.net>
2021-06-28 14:08:56 -0700

    Change color scheme to NASA blue and orange

--- src/App.js
+++ src/App.js
@@ -1,51 +1,56 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import './App.css';
-import SearchBar from './components/SearchBar'
-import SearchList from './components/SearchList'
+import SearchBar from './components/SearchBar';
+import SearchList from './components/SearchList';
 
 class App extends Component {
   state = {
     name: '',
-    data: []
-  }
+    data: [],
+  };
   componentDidMount() {
     try {
-      fetch('https://data.nasa.gov/resource/gh4g-9sfh.json').then((response)=> {
-      response.json().then((data) => {
-        if (data.error) {
-          console.log(data.error)
-        } else {
-          this.setState(()=>{
-            return ({ data: data})
-          })
+      fetch('https://data.nasa.gov/resource/gh4g-9sfh.json').then(
+        (response) => {
+          response.json().then((data) => {
+            if (data.error) {
+              console.log(data.error);
+            } else {
+              this.setState(() => {
+                return { data: data };
+              });
+            }
+          });
         }
-      })
-    })
-  } catch (e) {
-    console.log('Error loading data')
+      );
+    } catch (e) {
+      console.log('Error loading data');
+    }
   }
-}
 
-handleSearchChange = (name) => {
+  handleSearchChange = (name) => {
     this.setState(() => ({
-      name: name
-    }))
-
-}
+      name: name,
+    }));
+  };
   render() {
-    let filteredList = []
-    filteredList = this.state.data.filter(item => item.name.toLowerCase().includes(this.state.name.toLowerCase()))
-  return (
-    <div className="App">
-      <div className="App-header">
-      <h2>Meteorite Explorer</h2> 
+    let filteredList = [];
+    filteredList = this.state.data.filter((item) =>
+      item.name.toLowerCase().includes(this.state.name.toLowerCase())
+    );
+    return (
+      <div className="App">
+        <div className="search-panel">
+          <h1>Meteorite Explorer</h1>
+          <SearchBar handleSearchChange={this.handleSearchChange} />
+          {this.state.name !== '' ? (
+            <SearchList list={filteredList} />
+          ) : (
+            <SearchList list={this.state.data} />
+          )}
+        </div>
       </div>
-      <div className="search-panel">
-        <SearchBar handleSearchChange ={this.handleSearchChange} />
-        {this.state.name !== '' ? <SearchList list={filteredList}/> : <SearchList list={this.state.data} />}
-      </div>
-    </div>
-  );
+    );
   }
 }
 export default App;
